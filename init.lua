4686eb536012f282bceb5a6c6e00242cf709ed6b
Jakub Janór <kuba.janor@gmail.com>
2024-05-06 12:57:49 +0200

    Add harpoon

--- init.lua
+++ init.lua
@@ -218,6 +218,39 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
+  -- Harpoon
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      ---@diagnostic disable-next-line: missing-parameter
+      harpoon:setup()
+      local function map(lhs, rhs, opts)
+        vim.keymap.set('n', lhs, rhs, opts or {})
+      end
+      map('<leader>a', function()
+        harpoon:list():add()
+      end)
+      map('<leader>h', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+      map('<c-h><c-h>', function()
+        harpoon:list():select(1)
+      end)
+      map('<c-h><c-j>', function()
+        harpoon:list():select(2)
+      end)
+      map('<c-h><c-k>', function()
+        harpoon:list():select(3)
+      end)
+      map('<c-h><c-l>', function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
